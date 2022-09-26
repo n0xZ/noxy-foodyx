3dcb044af993229c68c1095527e23f348d03dd3b
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-09-26 15:14:54 -0300

    setup RQ provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { Provider } from 'react-redux'
+
 import App from './App'
-import 'unocss'
+import { store } from './redux/store'
+import 'uno.css'
 import '@unocss/reset/tailwind.css'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</QueryClientProvider>
 	</React.StrictMode>
 )
