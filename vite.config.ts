84cc810a64fb62607a7d90a739760217dcc1af85
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-09-26 13:56:15 -0300

    setup import aliases and unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import React from '@vitejs/plugin-react'
 import Unocss from 'unocss/vite'
+import { presetUno, presetAttributify } from 'unocss'
 import AutoImport from 'unplugin-auto-import/vite'
-// https://vitejs.dev/config/
+import { resolve } from 'path'
+
 export default defineConfig({
 	plugins: [
 		React(),
-		Unocss(),
+		Unocss({ presets: [presetUno(), presetAttributify()] }),
 		AutoImport({ imports: ['react-router-dom', 'react'] }),
 	],
+	resolve: {
+		alias: {
+			'~': resolve('src'),
+		},
+	},
 })
