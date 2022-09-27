2ac65860ae6b901a8c79d9771a9b5b73099feda7
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-09-27 16:48:11 -0300

    setup layouts and home outlet

---
+++ src/components/layout/LandingLayout.tsx
@@ -0,0 +1,40 @@
+import React, { ReactNode } from 'react'
+import { useAppSelector } from '~/redux/hooks'
+
+export default function LandingLayout({ children }: { children: ReactNode }) {
+	const { isLoading, userInfo } = useAppSelector((state) => state.auth)
+	return (
+		<div className='h-screen bg-sky-100'>
+			<header className="px-8 py-6 ">
+				<nav className="flex flex-row items-center justify-between container mx-auto max-w-5xl">
+					<h1>
+						<NavLink to="/">Proyix</NavLink>
+					</h1>
+					<ul className="flex flex-row items-center space-x-4">
+						{isLoading || (!isLoading && !userInfo) ? (
+							<>
+								<li>
+									<NavLink to="/login" className="px-8 py-4 rounded-lg bg-sky-400">
+										Iniciar sesión
+									</NavLink>
+								</li>
+								<li>
+									<NavLink to="/register">Unete ya!</NavLink>
+								</li>
+							</>
+						) : (
+							<>
+								<li>
+									<NavLink to="/home" className="px-8 py-4 rounded-lg bg-sky-400">
+										Home
+									</NavLink>
+								</li>
+							</>
+						)}
+					</ul>
+				</nav>
+			</header>
+			<main className="h-full">{children}</main>
+		</div>
+	)
+}
---
+++ src/components/layout/PrivateLayout.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from 'react'
+import { signOut } from 'firebase/auth'
+import { auth } from '~/lib/firebase'
+export default function PrivateLayout({ children }: { children: ReactNode }) {
+	const navigate = useNavigate()
+	const SignOut = () => {
+		navigate('/')
+		signOut(auth)
+	}
+	return (
+		<>
+			<header className="px-8 py-6 ">
+				<nav className="flex flex-row items-center justify-between container mx-auto max-w-5xl">
+					<h1>
+						<NavLink to="/home/general">Proyix</NavLink>
+					</h1>
+					<ul className="xl:flex flex-row items-center space-x-4 hidden">
+						<li>
+							<NavLink
+								to="/home/room"
+								className="px-8 py-4 rounded-lg hover:bg-sky-200 no-underline"
+							>
+								Crear nueva sala
+							</NavLink>
+						</li>
+						<li>
+							<button type="button" className="px-8 py-4 bg-sky-400" onClick={SignOut}>
+								Cerrar sesión
+							</button>
+						</li>
+					</ul>
+				</nav>
+			</header>
+			<main className="h-screen">{children}</main>
+		</>
+	)
+}
