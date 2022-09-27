247abcff37bdb95623fb09656dae5f6a49e3de63
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-09-27 16:47:55 -0300

    setup browser router routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,45 @@
 import { Suspense } from 'react'
-
-import {
-	RouterProvider,
-	createBrowserRouter,
-	createRoutesFromElements,
-	Outlet,
-} from 'react-router-dom'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from './lib/firebase'
 import { useAppDispatch } from './redux/hooks'
 import { removeUser, setUser } from './redux/reducers/auth'
+
 const Landing = lazy(() => import('./pages/landing'))
 const Login = lazy(() => import('./pages/login'))
 const Register = lazy(() => import('./pages/register'))
-const Home = lazy(() => import('./pages/home'))
-const Layout = () => {
-	return (
-		<>
-			<header className="px-8 py-4 ">
-				<nav className="flex flex-row items-center justify-between container mx-auto max-w-5xl">
-					<h1>
-						<NavLink to="/">Proyix</NavLink>
-					</h1>
-					<ul className="flex flex-row items-center space-x-4">
-						<li>
-							<NavLink to="/login">Iniciar sesión</NavLink>
-						</li>
-						<li>
-							<NavLink to="/register">Unete ya!</NavLink>
-						</li>
-					</ul>
-				</nav>
-			</header>
-			<main className="h-screen">
-				<Outlet />
-			</main>
-		</>
-	)
-}
-const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path="" element={<Layout />}>
-			<Route path="/" element={<Landing />} />
-			<Route path="login" element={<Login />} />
-			<Route path="register" element={<Register />} />
-			<Route path="home" element={<Home />} />
-		</Route>
-	)
-)
+const HomeOutlet = lazy(() => import('./pages/home.outlet'))
+const Home = lazy(() => import('./pages/home/index'))
+const Room = lazy(() => import('./pages/home/room/index'))
 function App() {
 	const dispatch = useAppDispatch()
-	const navigate = useNavigate()
+
 	onAuthStateChanged(auth, (user) => {
 		if (user) {
-			dispatch(setUser({ user }))
+			dispatch(
+				setUser({
+					userInfo: {
+						photoURL: user.photoURL,
+						displayName: user.displayName,
+						email: user.email,
+					},
+				})
+			)
 		}
 		if (!user) {
 			dispatch(removeUser())
-			navigate('/')
 		}
 	})
 	return (
 		<Suspense fallback={<div>Cargando pagina...</div>}>
-			<RouterProvider
-				router={router}
-				fallbackElement={<div>Cargando contenido de esta ruta....</div>}
-			/>
+			<Routes>
+				<Route path="/" element={<Landing />} />
+				<Route path="login" element={<Login />} />
+				<Route path="register" element={<Register />} />
+				<Route path="home" element={<HomeOutlet />}>
+					<Route path="general" element={<Home />} />
+					<Route path="room" element={<Room />} />
+				</Route>
+			</Routes>
 		</Suspense>
 	)
 }
