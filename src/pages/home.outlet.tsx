ccf18dfd5880ff5c562e021fa32265d8b230b7e1
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-10-10 00:26:18 -0300

    set new layout on home outlet page

--- src/pages/home.outlet.tsx
+++ src/pages/home.outlet.tsx
@@ -1,8 +1,66 @@
-import { useAppDispatch, useAppSelector } from '~/redux/hooks'
-import type { ReactNode } from 'react'
+import { useAppDispatch } from '~/redux/hooks'
 import { onAuthStateChanged, signOut } from 'firebase/auth'
 import { auth } from '~/lib/firebase'
 import { removeUser, setUser } from '~/redux/reducers/auth'
+
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { Icon } from '@iconify/react'
+
+function NavMenu() {
+	const navigate = useNavigate()
+	const dispatch = useAppDispatch()
+	const SignOut = () => {
+		navigate('/')
+		signOut(auth)
+	}
+	return (
+		<Menu
+			as="div"
+			className="relative xl:hidden lg:hidden inline-block text-left"
+		>
+			<div>
+				<Menu.Button className="inline-flex w-full justify-center items-center rounded-md bg-zinc-900 bg-opacity-60 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+					<Icon
+						icon="uil:align-justify"
+						className=" h-5 w-5 text-rose-200 hover:text-rose-100"
+						aria-hidden="true"
+					/>
+				</Menu.Button>
+			</div>
+			<Transition
+				as={Fragment}
+				enter="transition ease-out duration-100"
+				enterFrom="transform opacity-0 scale-95"
+				enterTo="transform opacity-100 scale-100"
+				leave="transition ease-in duration-75"
+				leaveFrom="transform opacity-100 scale-100"
+				leaveTo="transform opacity-0 scale-95"
+			>
+				<Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+					<div className="px-1 py-1 ">
+						<Menu.Item>
+							<NavLink
+								to="/home/recipes"
+								className="group flex w-full items-center rounded-md px-2 py-2 text-sm text-gray-900"
+							>
+								Buscar recetas
+							</NavLink>
+						</Menu.Item>
+						<Menu.Item>
+							<button
+								className="bg-orange-600 text-white group flex w-full items-center rounded-md px-2 py-2 text-sm"
+								onClick={() => SignOut()}
+							>
+								Cerrar sesión
+							</button>
+						</Menu.Item>
+					</div>
+				</Menu.Items>
+			</Transition>
+		</Menu>
+	)
+}
 
 export default function HomeOutlet() {
 	const navigate = useNavigate()
@@ -16,7 +74,7 @@ export default function HomeOutlet() {
 			dispatch(setUser({ userInfo: user }))
 		} else dispatch(removeUser())
 	})
-	console.log('Redireccionei')
+
 	return (
 		<>
 			<header className="px-8 py-4 ">
@@ -37,12 +95,13 @@ export default function HomeOutlet() {
 							<button
 								type="button"
 								className="px-8 py-4 rounded-lg  bg-orange-400 text-dark-50"
-								onClick={SignOut}
+								onClick={() => SignOut()}
 							>
 								Cerrar sesión
 							</button>
 						</li>
 					</ul>
+					<NavMenu />
 				</nav>
 			</header>
 			<main className="h-screen">
