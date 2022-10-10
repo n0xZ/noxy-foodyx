37d4083538f1e2f8a97e73f648c02805c98594e8
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-10-10 16:53:51 -0300

    setup recipes skeleton

---
+++ src/components/skeleton/RecipesSkeleton.tsx
@@ -0,0 +1,46 @@
+export function RecipesSkeleton() {
+	return (
+		<article className="grid xl:grid-cols-2 grid-cols-1 place-items-center container mx-auto gap-7">
+			<aside className="h-xl w-96 rounded-md bg-light-100  flex flex-col  justify-between items-center text-center p-2 space-y-2">
+				<div className="flex flex-col justify-center w-full h-full top-0 animate-pulse">
+					<div className="rounded-lg w-full h-64  bg-gray-300"></div>
+					<p className="font-bold h-12 bg-gray-300 h-12 mb-6 mt-4"></p>
+					<div className=" h-full bg-gray-300 "></div>
+				</div>
+				<button className="px-8 py-4 rounded-lg bg-light-500 w-full text-orange-400 hover:bg-orange-200 duration-100 ease-in-out">
+					Loading...
+				</button>
+			</aside>
+			<aside className="h-xl w-96 rounded-md bg-light-100  flex flex-col  justify-between items-center text-center p-2 space-y-2">
+				<div className="flex flex-col justify-center w-full h-full top-0 animate-pulse">
+					<div className="rounded-lg w-full h-64  bg-gray-300"></div>
+					<p className="font-bold h-12 bg-gray-300 h-12 mb-6 mt-4"></p>
+					<div className=" h-full bg-gray-300 "></div>
+				</div>
+				<button className="px-8 py-4 rounded-lg bg-light-500 w-full text-orange-400 hover:bg-orange-200 duration-100 ease-in-out">
+					Loading...
+				</button>
+			</aside>
+			<aside className="h-xl w-96 rounded-md bg-light-100  flex flex-col  justify-between items-center text-center p-2 space-y-2">
+				<div className="flex flex-col justify-center w-full h-full top-0 animate-pulse">
+					<div className="rounded-lg w-full h-64  bg-gray-300"></div>
+					<p className="font-bold h-12 bg-gray-300 h-12 mb-6 mt-4"></p>
+					<div className=" h-full bg-gray-300 "></div>
+				</div>
+				<button className="px-8 py-4 rounded-lg bg-light-500 w-full text-orange-400 hover:bg-orange-200 duration-100 ease-in-out">
+					Loading...
+				</button>
+			</aside>
+			<aside className="h-xl w-96 rounded-md bg-light-100  flex flex-col  justify-between items-center text-center p-2 space-y-2">
+				<div className="flex flex-col justify-center w-full h-full top-0 animate-pulse">
+					<div className="rounded-lg w-full h-64  bg-gray-300"></div>
+					<p className="font-bold h-12 bg-gray-300 h-12 mb-6 mt-4"></p>
+					<div className=" h-full bg-gray-300 "></div>
+				</div>
+				<button className="px-8 py-4 rounded-lg bg-light-500 w-full text-orange-400 hover:bg-orange-200 duration-100 ease-in-out">
+					Loading...
+				</button>
+			</aside>
+		</article>
+	)
+}
