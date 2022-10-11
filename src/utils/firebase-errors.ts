30572de63b27382f8ce61ea5b8e5826f624c5623
n0xZ <o.gonzalo@hotmail.com>
2022-10-10 23:50:06 -0300

    add new firebase error exception

--- src/utils/firebase-errors.ts
+++ src/utils/firebase-errors.ts
@@ -1,10 +1,8 @@
 export const handleFirebaseErrors = (error?: string) => {
-  if (!error)
-    return
-  if (error?.includes('auth/user-not-found'))
-    return 'Usuario no encontrado'
-  if (error?.includes('auth/email-already-in-use'))
-    return 'Email ya en uso'
-  if (error?.includes('auth/wrong-password'))
-    return 'Contraseña incorrecta'
+	if (!error) return
+	if (error?.includes('auth/user-not-found')) return 'Usuario no encontrado'
+	if (error?.includes('auth/email-already-in-use')) return 'Email ya en uso'
+	if (error?.includes('auth/wrong-password')) return 'Contraseña incorrecta'
+	if (error?.includes('auth/network-request-failed'))
+		return 'Al parecer hubo un error relacionado a tu conexión. Por favor, vuelve a intentarlo más tarde.'
 }
